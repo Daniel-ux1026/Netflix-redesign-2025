320db2f5d7a721df7404fa0df5874128b416356a
Daniel Olivos <olivosd01@gmail.com>
2025-10-02 18:20:35 -0500

    feat(top-nav): estructura base del prototipo + barra superior fija

---
+++ app/src/main.js
@@ -0,0 +1,15 @@
+const app = document.getElementById('app');
+app.innerHTML = `
+  <section class="row">
+    <h2>Seguir viendo</h2>
+    <div tabindex="0" class="card"></div>
+    <div tabindex="0" class="card"></div>
+    <div tabindex="0" class="card"></div>
+  </section>
+  <section class="row">
+    <h2>Top 10</h2>
+    <div tabindex="0" class="card"></div>
+    <div tabindex="0" class="card"></div>
+    <div tabindex="0" class="card"></div>
+  </section>
+`;
